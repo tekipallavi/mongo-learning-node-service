ab409ba3d6b6781ecd02b2d459ee22932a0cb948
tekipallavi <tekipallavi@gmail.com>
2025-10-29 15:44:00 +0530

    Added routes to express

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 let app = express();
 
+const employeeRoutes = require("src/routes/employee.routes.js");
+const employeeRoutesV2 = require("src-v2/routes/employee.routes.js");
+
 async function startDb() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -21,8 +24,14 @@ loadService = () => {
   app.use(bodyParser.json());
   app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
   app.use(express.json()); // To parse JSON request bodies
+
+  //add routes
+
+  app.use('/employee', employeeRoutes);
+  app.use('/employee-v2', employeeRoutesV2);
 }
 
 app.listen(process.env.PORT || 3001, () => {
   console.log("start listening!!");
+
 });
