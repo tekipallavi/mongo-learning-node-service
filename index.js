903763676b6db995bea3a5da940da3b41c4f239d
PT-CTS <pptravels@gmail.com>
2025-11-20 07:22:02 +0530

    Added Projects generator.

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const {client: mongoClient } = require('./db/db.connection');
+const { client: mongoClient } = require("./db/db.connection");
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 let app = express();
-const {createFakeEmployees, setEmployeeDetails} = require('./src/controllers/employee.controller.js')
+const {
+  createFakeEmployees,
+  setEmployeeDetails,
+} = require("./src/controllers/employee.controller.js");
+const {
+  createProjects,
+  countProjectDocuments,
+} = require("./src/controllers/project.controller.js");
 
 const employeeRoutes = require("./src/routes/employee.routes.js");
 const employeeRoutesV2 = require("./src-v2/routes/employee.routes.js");
@@ -15,7 +22,6 @@ async function startDb() {
     // Connect the client to the server	(optional starting in v4.7)
     await mongoClient.connect();
     loadService();
-
   } finally {
     // Ensures that the client will close when you finish/error
     //await mongoClient.close();
@@ -23,17 +29,17 @@ async function startDb() {
 }
 
 startDb();
-loadService = () => {
+loadService = async () => {
   app.use(bodyParser.json());
-  app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+  app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
   app.use(express.json()); // To parse JSON request bodies
 
   //add routes
 
-  app.use('/employee', employeeRoutes);
-  app.use('/employee-v2', employeeRoutesV2);
-  
-  app.use('/project', projectRoutes);
+  app.use("/employee", employeeRoutes);
+  app.use("/employee-v2", employeeRoutesV2);
+
+  app.use("/project", projectRoutes);
 
   // testing
   // setEmployeeDetails({body:{
@@ -41,9 +47,10 @@ loadService = () => {
   //   techStack: 'tech',
   //   experience: 5
   // }});
-}
+  //createProjects(1000);
+  countProjectDocuments();
+};
 
 app.listen(process.env.PORT || 3001, () => {
   console.log("start listening!!");
-
 });
