fa0a06db749b890fc32d8dcc10a76dc37b533646
PT <pptravels08@gmail.com>
2025-12-24 11:46:37 +0000

    Add query1 function to learning-v2.js and update index.js to include it

--- learn/learning-v2.js
+++ learn/learning-v2.js
@@ -2,66 +2,57 @@ const { db } = require('../db/db.connection');
 const { collections } = require('../utils');
 
 async function explainAggregation(collection, pipeline, opts = {}) {
-  const { verbosity = 'executionStats', allowDiskUse = false, maxTimeMS } = opts;
-  const cursor = collection.aggregate(pipeline, { allowDiskUse, maxTimeMS });
-  return await cursor.explain(verbosity);
+    const { verbosity = 'executionStats', allowDiskUse = false, maxTimeMS } = opts;
+    const cursor = collection.aggregate(pipeline, { allowDiskUse, maxTimeMS });
+    return await cursor.explain(verbosity);
 }
 
-const empPerformMetricsv2_me = async () => {
+//query-1
+
+const query1 = async () => {
     try {
-        const result = await db.collection(collections.employee).aggregate([
-            { $match: { experience: { $gte: 3 } } },
+        const result = await db.collection(collections.interest).aggregate([
             {
-                $lookup:
-                {
-                    from: collections.interest,
-                    localField: 'employeeID',
-                    foreignField: 'employeeId',
-                    as: 'interestDetails'
+                $lookup: {
+                    from: collections.employee,
+                    localField: 'employeeId',
+                    foreignField: 'employeeID',
+                    as: 'employee'
                 }
             },
-            { $addFields: { projectIds: { $map: { input: "$interestDetails", as: "interest", in: "$$interest.projectId" } } } },
-            //{$lookup: {from :collections.project, localField: 'projectIds', foreignField: 'projectId', as: 'projectDetails'}},
+            { $unwind: "$employee" },
             {
-                $lookup:
-                {
+                $match: { "employee.experience": { $gte: 3 } }
+            },
+            {
+                $lookup: {
                     from: collections.project,
-                    let: { pIds: "$projectIds", eTechStack: '$techStack' },
-                    pipeline: [
-                        { $match: { $expr: { $in: ['$projectId', '$$pIds'] } } },
-                        { $project: { _id: 0, techStack: 1 } },
-                        //{$addFields: {techStackIntersection: {$size:{$setIntersection: ['$techStack', '$$eTechStack']}}}},
-                        {
-                            $addFields: {
-                                matchPercent:
-                                {
-                                    $cond: [
-                                        { $gt: [{$size: '$techStack'}, 0] },
-                                        { $divide: [{ $size: { $setIntersection: ['$techStack', '$$eTechStack'] } }, {$size: '$techStack'}] },
-                                        0
-                                    ],
-                                },
-                            },
-                        },
-                    ],
-                    as: 'projectTechStack'
+                    localField: 'projectId',
+                    foreignField: 'projectId',
+                    as: 'project'
                 }
             },
+            { $unwind: "$project" },
             {
                 $addFields: {
-                    avgTechOverlap: {$ifNull: [{ $avg: "$projectTechStack.matchPercent"}, 0]}
+                    avgTechOverlap: {
+                        $divide: [{ $size: { $setIntersection: ["$employee.techStack", "$project.techStack"] } }, { $size: "$project.techStack" }]
+
+                    }
                 }
             },
-            {$project: {employeeID:1, name:1, techStack:1, avgTechOverlap:1, projectIntrestsCount:1}}
+            {$project: {'projectId':1, 'employeeId':1, 'avgTechOverlap':1, 'project.projectId':1, 'employee.employeeID': 1 }}
 
         ]).toArray();
-        console.log("Employee performance metrics v2", result);
-    } catch (e) {
-        console.error(e);
-    }
 
+        console.log(result);
+    } catch (error) {
+        console.error('Error in query1:', error);
+        throw error;
+    }
 }
 
+/*
 const empPerformMetricsv2 = async() => {
     try {
         const result = await db.collection(collections.interest).aggregate([
@@ -110,64 +101,5 @@ const empPerformMetricsv2 = async() => {
     } catch (e) {
         console.error(e);
     }
-}
-
-const employeeExecutionStats = async () => {
-    try {
-
-        const pipeline = [
-            { $match: { experience: { $gte: 3 } } },
-            { $addFields: { projectIntrestsCount: { $size: "$interests" } } },
-            {
-                $lookup:
-                {
-                    from: collections.interest,
-                    localField: 'employeeID',
-                    foreignField: 'employeeId',
-                    as: 'interestDetails'
-                }
-            },
-            { $addFields: { projectIds: { $map: { input: "$interestDetails", as: "interest", in: "$$interest.projectId" } } } },
-            //{$lookup: {from :collections.project, localField: 'projectIds', foreignField: 'projectId', as: 'projectDetails'}},
-            {
-                $lookup:
-                {
-                    from: collections.project,
-                    let: { pIds: "$projectIds", eTechStack: '$techStack' },
-                    pipeline: [
-                        { $match: { $expr: { $in: ['$projectId', '$$pIds'] } } },
-                        { $project: { _id: 0, techStack: 1 } },
-                        //{$addFields: {techStackIntersection: {$size:{$setIntersection: ['$techStack', '$$eTechStack']}}}},
-                        {
-                            $addFields: {
-                                matchPercent:
-                                {
-                                    $cond: [
-                                        { $gt: [{$size: '$techStack'}, 0] },
-                                        { $divide: [{ $size: { $setIntersection: ['$techStack', '$$eTechStack'] } }, {$size: '$techStack'}] },
-                                        0
-                                    ],
-                                },
-                            },
-                        },
-                    ],
-                    as: 'projectTechStack'
-                }
-            },
-            {
-                $addFields: {
-                    avgTechOverlap: {$ifNull: [{ $avg: "$projectTechStack.matchPercent"}, 0]}
-                }
-            },
-            {$project: {employeeID:1, name:1, techStack:1, avgTechOverlap:1, projectIntrestsCount:1}}
-
-        ];
-        
-        const explanation = await explainAggregation(db.collection(collections.interest), pipeline, { verbosity: 'executionStats' });
-        console.log(JSON.stringify(explanation, null, 2));
-    } catch (e) {
-        console.error(e);   
-    }
-}
-
-module.exports = { empPerformMetricsv2: empPerformMetricsv2 }
+} */
+module.exports = { query1 }
